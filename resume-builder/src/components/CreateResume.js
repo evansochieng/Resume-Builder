9672f1acd6cd0a64024dd85fd583a51c045e65bb
Evans Ochieng <evansochieng121@gmail.com>
2022-10-05 11:07:57 +0300

    Created skeleton of all components

--- resume-builder/src/components/CreateResume.js
+++ resume-builder/src/components/CreateResume.js
@@ -1,1 +1,7 @@
 import React from "react";
+
+function CreateResume(){
+
+}
+
+export default CreateResume;
--- resume-builder/src/components/NavBar.js
+++ resume-builder/src/components/NavBar.js
@@ -1,1 +1,7 @@
 import React from "react";
+
+function NavBar(){
+
+}
+
+export default NavBar;
