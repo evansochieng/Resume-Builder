eef8ae9efd81bd6c7c8ad5189790c177e09d5b02
Evans Ochieng <evansochieng121@gmail.com>
2022-10-05 11:19:42 +0300

    import components in app component

--- resume-builder/src/App.js
+++ resume-builder/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// Import the other components
+import NavBar from "./components/NavBar";
+import SignIn from "./components/SignIn";
+import Home from "./components/Home";
+import CreateResume from "./components/CreateResume";
+import SignOut from "./components/SignOut";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
 }
 
 export default App;
