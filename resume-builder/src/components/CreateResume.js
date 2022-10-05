40290ac182d9214f895f6d3d0bb4c0614030dc14
Evans Ochieng <evansochieng121@gmail.com>
2022-10-05 12:50:12 +0300

    Finish adding create-resume component contents

--- resume-builder/src/components/CreateResume.js
+++ resume-builder/src/components/CreateResume.js
@@ -6,12 +6,14 @@ function CreateResume(){
             <form className="resume">
                 {/* Information about myself */}
                 <div className="about">
+                    <h3>Information about myself</h3>
                     <label htmlFor="about">About Myself</label>
                     <input type="text" name="about"/>
                 </div>
 
                 {/* Contact details: official name, githubUsername, phone, email */}
                 <div className="contact">
+                    <h3>Contact Details</h3>
                     <label htmlFor="name">Name</label>
                     <input type="text" name="name"/>
                     <label htmlFor="email">Email</label>
@@ -22,14 +24,30 @@ function CreateResume(){
 
                 {/* Education */}
                 <div className="education">
+                    <h3>Academic Qualifications</h3>
                     <label htmlFor="education">Academic Qualifications</label>
                     <input type="text" name="education" />
                 </div>
 
+                {/* Skills and Abilities */}
+                <div className="skills">
+                    <h3>Skills and Abilities</h3>
+                    <label htmlFor="skills">Skills</label>
+                    <input type="text" name="skills" id="skills" />
+                </div>
+
                 {/* Work Experience */}
-                <div>
-                    <label htmlFor="experience"></label>
+                <div className="experience">
+                    <h3>Work Experience</h3>
+                    <label htmlFor="experience">Work Experience</label>
                     <input type="text" name="experience" id="experience" />
+                </div>
+                
+                {/* Projects */}
+                <div className="projects">
+                    <h3>Projects</h3>
+                    <label htmlFor="projects">Projects</label>
+                    <input type="text" name="projects" id="projects"/>
                 </div>
             </form>
         </div>
